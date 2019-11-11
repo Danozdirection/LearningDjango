c115dcab2ecdc54b84d90cfcd20f4eb7dec3ce55
Dan Erven <daniel.erven@gmail.com>
2019-11-11 22:47:58 +1300

    2

--- LearningDjango/HelloDjangoApp/views.py
+++ LearningDjango/HelloDjangoApp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    html_content = "<html><head><title>Hello, Django</title></head><body>"
+    html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+    html_content += "</body></html>"
+
+    return HttpResponse(html_content)
